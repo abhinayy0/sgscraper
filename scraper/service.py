e6a342915d55fafc9ecaa3847e1b2335cc3583c0
Abhinay Yadav <abhinayy0@gmail.com>
2022-04-24 23:08:58 +0530

    Added templates

--- scraper/service.py
+++ scraper/service.py
@@ -9,41 +9,48 @@ class ScrapeData(object):
 
     _HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
-    def __init__(self, querystring, backend) -> None:
+    def __init__(self, querystring) -> None:
         self.query = querystring
         self._urls_to_scrape = None
 
     def list_of_urls(self):
         raw_urls = set(search(self.query, tld= "co.in", num=20, start =0, stop=20, pause=2))
-
-        self._urls_to_scrape = list([url for url in raw_urls if ("glassdoor.com" in url and "SRCH_KT" in url)])
-        
+        scraped_urls = list([url for url in raw_urls if ("glassdoor.com" in url and "SRCH_KT" in url)])
+        if scraped_urls:
+            self._urls_to_scrape = scraped_urls[-1]
 
     def scrapingdata(self, scrapepage):
         '''scrapes data after takes html page as argument'''
+        if not scrapepage:
+            return []
         soup = BeautifulSoup(scrapepage, 'html.parser')
         results = soup.find(id='InterviewsSearchResults')
+       
 
         if results:
             return None
 
         authordata = results.find_all(class_='authorInfo')  # company names
         quedata = results.find_all(class_='questionText')   # interview questions
+        print(authordata, quedata)
         zipped = zip_longest(authordata, quedata, fillvalue='?')
         final_result = [(company_name.text, question.text) for company_name, question in list(zipped)]
-
+        print(final_result)
         return final_result
 
 
     def scraped(self):
         '''function to give scrape_page takes url to scrape as argument'''
         
-        scrapedpage = requests.get(self._urls_to_scrape, headers= ScrapeData.HEADERS)	
+        scrapedpage = requests.get(self._urls_to_scrape, headers= ScrapeData._HEADERS)	
         if scrapedpage.status_code == 200:
             return scrapedpage.content
 
     def start_scraping(self):
         self.list_of_urls()
+        print(self._urls_to_scrape)
+        self.scrapingdata(self.scraped())
+
 
         
         
@@ -64,3 +71,8 @@ class FileWriter(object):
             file_writer = csv.writer(file, self.delimitter, self.quotechar, quoting=csv.QUOTE_MINIMAL)
             file_writer.writerow(csvheader)
             file_writer.writerows(self.scraped_data)
+
+
+if __name__ == "__main__":
+    scraping = ScrapeData("python glassdoor interview questions")
+    scraping.start_scraping()
--- scraper/views.py
+++ scraper/views.py
@@ -1,7 +1,18 @@
 from starlette.responses import JSONResponse
 from starlette.endpoints import HTTPEndpoint
+from starlette.templating import Jinja2Templates
+from scraper.service import ScrapeData
+
+templates = Jinja2Templates(directory='templates')
 
 class ScrapeView(HTTPEndpoint):
 
     async def get(self, request):
-        return JSONResponse({"message":"hello"})
+        return templates.TemplateResponse('index.html', {'request': request})
+
+    async def post(self, request):
+        data =await request.form()
+        scraping = ScrapeData(data["keyword"])
+        scraping.start_scraping()
+
+        return templates.TemplateResponse('searching.html', {'request': request})
